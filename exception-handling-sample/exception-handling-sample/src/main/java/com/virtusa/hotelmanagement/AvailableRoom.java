39eb6b9b39070b73f6730569eca0cfa1bf1d5f9c
Madhushika Madhubhani <33512791+madhushhika@users.noreply.github.com>
2021-01-19 14:15:38 +0530

    updated with packages

--- exception-handling-sample/exception-handling-sample/src/main/java/com/virtusa/hotelmanagement/AvailableRoom.java
+++ exception-handling-sample/exception-handling-sample/src/main/java/com/virtusa/hotelmanagement/AvailableRoom.java
@@ -2,12 +2,10 @@ package com.virtusa.hotelmanagement;
 import com.virtusa.exception.RoomCountRuntimeException;
 
 public class AvailableRoom {
-	
-	
 	 //Unchecked Exception
 	public  void calRoomCount( int availableRoomCount) throws RoomCountRuntimeException {
 	  
-          if (availableRoomCount>5) {
+		if (availableRoomCount>5) {
         	  
         	  throw new  RoomCountRuntimeException("Rooms are not available");
            }
