8238151724e518375607e6d7981b7d37b926ea3b
Madhushika Madhubhani <33512791+madhushhika@users.noreply.github.com>
2021-01-19 14:11:22 +0530

    updated with packages

---
+++ employee-mangement-sample/com/virtusa/ems/application/Application.java
@@ -0,0 +1,12 @@
+
+public class Application{
+
+public static void main (String[]args){
+
+   Employee employee = new Employee ();
+   employee.print();
+   Manager manager = new Manager();
+   manager.print();  
+ }
+
+}
