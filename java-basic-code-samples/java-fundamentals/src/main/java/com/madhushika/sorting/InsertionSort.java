b872e6019179605e3fe5ca5379c33341eec16046
Madhushika Madhubhani <33512791+madhushika-madhubhani@users.noreply.github.com>
2021-01-23 11:22:47 +0530

    add comment

--- java-basic-code-samples/java-fundamentals/src/main/java/com/madhushika/sorting/InsertionSort.java
+++ java-basic-code-samples/java-fundamentals/src/main/java/com/madhushika/sorting/InsertionSort.java
@@ -7,15 +7,15 @@ class InsertionSort {
 	{ 
 		int n = arr.length; 
 		for (int i = 1; i < n; ++i) { 
-			int key = arr[i]; 
-			int j = i - 1; 
+			int key = arr[i];     //key is the current value
+			int j = i - 1; //start from the previous value
 
 			/* Move elements of arr[0..i-1], that are 
 			greater than key, to one position ahead 
 			of their current position */
 			while (j >= 0 && arr[j] > key) { 
-				arr[j + 1] = arr[j]; 
-				j = j - 1; 
+				arr[j + 1] = arr[j];  //copy to the right side
+				j = j--; 
 			} 
 			arr[j + 1] = key; 
 		} 
