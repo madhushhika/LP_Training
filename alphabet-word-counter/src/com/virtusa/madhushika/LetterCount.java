28b28879e6d63c0e7c8ed989979feb7b59d7f726
Madhushika Madhubhani <33512791+madhushika-madhubhani@users.noreply.github.com>
2021-01-31 20:25:10 +0530

    added counter

---
+++ alphabet-word-counter/src/com/virtusa/madhushika/LetterCount.java
@@ -0,0 +1,37 @@
+package com.virtusa.madhushika;
+
+import java.util.InputMismatchException;
+
+public class LetterCount {
+	private int sum;
+	private int wordCount = 0;
+
+	public void letterforEach(String word) throws NullPointerException,InputMismatchException {
+		if ("".equals(word)) {
+			throw new NullPointerException();
+		}
+
+		for (char character : word.toCharArray()) {
+			if (!Character.isAlphabetic(character)){
+				throw new InputMismatchException();
+			}
+			if (character >= 'A' && character <= 'Z') {
+				sum += 1 + character - 'A';
+			}
+		}
+		System.out.printf("The sum   is %d%n", sum);
+
+		for (char letter = 'A'; letter <= 'Z'; letter++) {
+			for (int i = 0; i < word.length(); i++) {
+				if (word.charAt(i) == letter) {
+					wordCount++;
+				}
+			}
+			if (wordCount > 0) {
+				System.out.println(letter + "=" + wordCount);
+				wordCount = 0;
+			}
+		}
+	}
+
+}
