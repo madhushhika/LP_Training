e26877049229e7f4dab8a042690eb9243d3836cc
Madhushika Madhubhani <33512791+madhushika-madhubhani@users.noreply.github.com>
2021-02-08 18:01:42 +0530

    Delete design-pattern-samples directory

--- design-pattern-samples/src/main/java/com/madhushika/factory/WeddingPhoto.java
+++
@@ -1,9 +0,0 @@
-package com.virtusa.factory;
-
-public class WeddingPhoto extends Photography {
-
-	public String toString() {
-		return "Wedding Photography";
-	}
-
-}
--- design-pattern-samples/src/main/java/com/madhushika/singleton/Application.java
+++
@@ -1,9 +0,0 @@
-package com.virtusa.singleton;
-
-public class Application {
-	public static void main(String[] args) {
-		Pin genaratePin = new Pin();
-		System.out.println(genaratePin);
-
-	}
-}
